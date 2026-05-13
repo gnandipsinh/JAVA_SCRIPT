41237331db0ac1ce1426d1c770d2e69eda5e67a5
Dharmik Ahir <dharmik@example.com>
2026-05-13 14:25:13 +0530

    variable

---
+++ 02_Js_intro_basic_concept/02_let.js
@@ -0,0 +1,22 @@
+function letVariable() {
+  let num1 = "hello dear";
+
+  if (true) {
+    let num2 = "where are you going";
+    console.log(num2);
+  }
+
+  console.log(num1);
+}
+
+letVariable();
+
+let bike;
+
+bike = "hero";
+
+console.log("This is new bike ", bike);
+
+bike = "honda";
+
+console.log("This is new bike ", bike);
