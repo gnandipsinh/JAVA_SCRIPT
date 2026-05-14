cb15863250ace9c46316aea00ba32c1a4c5b8665
Dharmik Ahir <dharmik@example.com>
2026-05-14 14:09:58 +0530

    new code added

---
+++ 02_Js_intro_basic_concept/03_const.js
@@ -0,0 +1,28 @@
+function student1() {
+  const name1 = "My name is gohil gnandipsinh";
+
+  console.log("your name is ? \n", name1);
+
+  function student2() {
+    const name2 = " I am coder";
+
+    console.log(name2);
+  }
+
+  student2();
+  
+}
+
+student1();
+
+const num = 10;
+
+const std = {
+  name: "gohil gnandipsinh",
+};
+
+console.log("std.name");
+
+std.name = "abc";
+
+console.log(std.name);
