ff98e85844413f3aeea0883735dfb427ade55bb3
Dharmik Ahir <dharmik@example.com>
2026-05-26 13:37:29 +0530

    new code added

---
+++ 05_code_execution_js/04_hoisting.js
@@ -0,0 +1,15 @@
+play();
+
+function play(game)
+{
+    console.log("i am playing cricket");
+}
+
+hoistedVar="exercise";
+
+
+console.log(hoistedVar);
+
+var hoistedVariable;
+
+
---
+++ 06_string_method_js/01_string_method.js
@@ -0,0 +1,56 @@
+let name = "gnandipisnh";
+
+let text = "hello gohil";
+
+console.log("text:", text);
+
+console.log("char at:", text.charAt(4));
+
+console.log("joined string:", text.concat(" good morning"));
+
+console.log("end with:", text.endsWith("l"));
+
+console.log("start with:", text.startsWith("g"));
+
+console.log(
+  "my name is gohil gnandipisnh and i am currently study in bca:",
+  text.includes("bca"),
+);
+
+console.log("index of :", text.indexOf("gohil"));
+
+// Example 2
+
+let lan = "javascript language";
+
+console.log("text:", lan);
+
+console.log("char at:", lan.charAt(3));
+
+console.log("joined string:", lan.concat(" is easy"));
+
+console.log("end with:", lan.endsWith("e"));
+
+console.log("start with:", lan.startsWith("j"));
+
+console.log("includes:", lan.includes("language"));
+
+console.log("index of:", lan.indexOf("script"));
+
+// Example 3
+
+let student = "welcome student";
+
+console.log("text:", student);
+
+console.log("char at:", student.charAt(5));
+
+console.log("joined string:", student.concat(" good morning"));
+
+console.log("end with:", student.endsWith("t"));
+
+console.log("start with:", student.startsWith("w"));
+
+console.log("includes:", student.includes("student"));
+
+console.log("index of:", student.indexOf("come"));
