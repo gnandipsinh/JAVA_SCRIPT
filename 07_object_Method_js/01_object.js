68443024e60af5fd6568a4176cd1419f5870d1de
Dharmik Ahir <dharmik@example.com>
2026-05-28 13:37:02 +0530

    new code

---
+++ 07_object_Method_js/01_object.js
@@ -0,0 +1,25 @@
+
+
+const personalDetails=
+{
+    name :"gohil gnandipisnh",
+    age:19,
+
+};
+
+console.log(typeof personalDetails);
+
+console.log("personDetails", personalDetails);
+
+
+const studentDetails = new Object();
+
+console.log(typeof studentDetails);
+
+studentDetails.totalStudent = 500;
+
+studentDetails.exam = "common entrance test";
+
+studentDetails.block = "A-E";
+
+console.log("studentDetails", studentDetails);
