3e57def1bcb3294ea262afb142cbb2edbf055960
Dharmik Ahir <dharmik@example.com>
2026-05-28 13:39:31 +0530

    new code added

--- 07_object_Method_js/01_object.js
+++ 07_object_Method_js/01_object.js
@@ -23,3 +23,55 @@ studentDetails.exam = "common entrance test";
 studentDetails.block = "A-E";
 
 console.log("studentDetails", studentDetails);
+
+// exampale 2
+const mobileDetails =
+{
+    brand : "Samsung",
+    model : "Galaxy M14",
+    price : 14999,
+};
+
+console.log(typeof mobileDetails);
+
+console.log("mobileDetails", mobileDetails);
+
+
+const employeeDetails = new Object();
+
+console.log(typeof employeeDetails);
+
+employeeDetails.employeeName = "Rahul";
+
+employeeDetails.department = "IT";
+
+employeeDetails.salary = 35000;
+
+console.log("employeeDetails", employeeDetails);
+
+
+// example 3
+
+const carDetails =
+{
+    brand : "Toyota",
+    model : "Fortuner",
+    color : "Black",
+};
+
+console.log(typeof carDetails);
+
+console.log("carDetails", carDetails);
+
+
+const hospitalDetails = new Object();
+
+console.log(typeof hospitalDetails);
+
+hospitalDetails.hospitalName = "City Care Hospital";
+
+hospitalDetails.totalDoctors = 120;
+
+hospitalDetails.location = "Ahmedabad";
+
+console.log("hospitalDetails", hospitalDetails);
